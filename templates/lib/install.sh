d7ae39d7b28592e066611d5dd87cc6b10493e9d1
Dan Reeves <hey@danreev.es>
2018-11-06 09:43:57 +0000

    Windows support (Travis CI) (#21)
    * Add Windows env and only use cross on linux
    
    Signed-off-by: Dan Reeves <hey@danreev.es>
    
    * Fix bash and run crossgen on Windows too
    
    Signed-off-by: Dan Reeves <hey@danreev.es>
    
    * Missed one
    
    Signed-off-by: Dan Reeves <hey@danreev.es>
    
    * Revert crossgen building in Windows :(
    
    Signed-off-by: Dan Reeves <hey@danreev.es>

--- templates/lib/install.sh
+++ templates/lib/install.sh
@@ -30,18 +30,10 @@ main() {
       ;;
   esac
 
-  # This fetches latest stable release
-  local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-    | cut -d/ -f3 \
-    | grep -E '^v[0.1.0-9.]+$' \
-    | $sort --version-sort \
-    | tail -n1)
-  curl -LSfs https://japaric.github.io/trust/install.sh | \
-    sh -s -- \
-    --force \
-    --git japaric/cross \
-    --tag $tag \
-    --target $target
+  # Install cross for linux cross compilation
+  if [ $TRAVIS_OS_NAME = linux ]; then
+    cargo install cross --force
+  fi
 
   # Install test dependencies
   rustup component add rustfmt-preview
